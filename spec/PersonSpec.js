d23da917d6d92817667731e541d05c8f13d6b2ca
davidwkaiser <davidwkaiser@yahoo.com>
2017-08-12 17:12:30 -0500

    clean up

--- spec/PersonSpec.js
+++ spec/PersonSpec.js
@@ -1,18 +1,3 @@
-// describe('Person', function () {
-//     it('calls the sayHello() function', function () {
-//         var fakePerson = new Person();
-//         spyOn(fakePerson, 'sayHello');
-//         fakePerson.helloSomeone('world');
-//         expect(fakePerson.sayHello).toHaveBeenCalled();
-//   });
-//     it('greets the world', function(){
-//       var fakePerson = new Person();
-//       spyOn(fakePerson, 'helloSomeone');
-//       fakePerson.helloSomeone('world');
-//       expect(fakePerson.helloSomeone).toHaveBeenCalledWith('world');
-//     });
-// });
-
 describe('Person', function () {
   var fakePerson
   beforeEach(function(){
