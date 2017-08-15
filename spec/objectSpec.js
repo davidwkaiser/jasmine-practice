7f98f4dbf0405324c49713c96e482859043dc43e
davidwkaiser <davidwkaiser@yahoo.com>
2017-08-14 22:48:02 -0500

    add some fun things

---
+++ spec/objectSpec.js
@@ -0,0 +1,11 @@
+describe("My new object", function(){
+  it("returns double the number given", function(){
+    expect(10).toEqual(thing.double(5))
+  })
+
+  it("returns triple the number given", function(){
+    expect(12).toEqual(thing.triple(4))
+  })
+})
+
+
